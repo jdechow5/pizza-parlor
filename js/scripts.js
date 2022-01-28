816ed9c1aab9b359258e59db0c15cee91901c56c
Justin Dechow <justin.dechow@gmail.com>
2022-01-28 09:52:18 -0800

    WIP: calculatePrice is returning the wrong values

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,21 @@ Pizza.prototype.calculatePrice = function () {
   const basePrice = 10;
   let addToppings = 0;
   let sizeChoice = 0;
-}
 
+
+  this.toppings.forEach(function (topping) {
+    if (topping === 'mushrooms' || topping === 'black olives' || topping === 'jalapenos' || topping === 'onions' || topping === 'garlic') {
+      addToppings += 2;
+    } else if (topping === 'sausage' || topping === 'bacon' || topping === 'pepperoni' || topping === 'anchovies') {
+      addToppings += 3;
+    }
+  });
+
+  if (this.size === 'medium') {
+    sizeChoice += 3;
+  } else if (this.size === 'large') {
+    sizeChoice += 5;
+  }
+  console.log(addToppings);
+  return basePrice + addToppings + sizeChoice;
+}
