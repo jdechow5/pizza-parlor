6d0c5755815827b0ba9992f46b34a4de86d5e294
Justin Dechow <justin.dechow@gmail.com>
2022-01-28 10:50:49 -0800

    add HTML to our index.html file

--- js/scripts.js
+++ js/scripts.js
@@ -14,16 +14,15 @@ Pizza.prototype.calculatePrice = function () {
   this.toppings.forEach(function (topping) {
     if (topping === 'mushrooms' || topping === 'black olives' || topping === 'jalapenos' || topping === 'onions' || topping === 'garlic') {
       addToppings += 2;
-    } else if (topping === 'sausage' || topping === 'bacon' || topping === 'pepperoni' || topping === 'anchovies') {
+    } if (topping === 'sausage' || topping === 'bacon' || topping === 'pepperoni' || topping === 'anchovies') {
       addToppings += 3;
     }
   });
 
   if (this.size === 'medium') {
     sizeChoice += 3;
-  } else if (this.size === 'large') {
+  } if (this.size === 'large') {
     sizeChoice += 5;
   }
-  console.log(addToppings);
   return basePrice + addToppings + sizeChoice;
 }
