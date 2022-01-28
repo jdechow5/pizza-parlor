a79dc13b26bc0f891dc1c8fff2a886ba7a4a2956
Justin Dechow <justin.dechow@gmail.com>
2022-01-28 09:26:00 -0800

    add prototype.calculatePrice, although added to constructor need to write logic to actually check price

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,10 @@ function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
+
+Pizza.prototype.calculatePrice = function () {
+  const basePrice = 10;
+  let addToppings = 0;
+  let sizeChoice = 0;
+}
+
